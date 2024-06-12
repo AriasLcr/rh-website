787c19d21b3736b0ac44922bacdf4b7d822f849f
AriasLcr <ariaslcr@gmail.com>
2024-06-12 16:23:38 -0500

    Removed react-fade-in, replaced with framer-motion.

--- website/src/app/components/hero/hero.tsx
+++ website/src/app/components/hero/hero.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import Navbar from "../navbar/navbar";
-import FadeIn from "react-fade-in";
 import Image from "next/image";
 
 export default function Hero() {
   return (
     <div className="w-full flex bg bg-gray-950 justify-center items-center flex-col h-[32rem]">
-      <FadeIn delay={200}>
         <h1 className="text-white font-bold text-9xl">Rh+</h1>
         <h2 className="text-white  text-3xl">Now <b>YOU</b> can be <b>SILLY</b></h2>
-      </FadeIn>
       {/* <Image /> */}
     </div>
   );
